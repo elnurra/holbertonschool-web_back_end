c1a2440b2b1322945356b6811cf5f38e1662b5e7
elnurra <elnurra@code.edu.az>
2024-05-02 09:59:38 +0400

    feat: bug-fix

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """1st Element of a Sequence"""
-from typing import Any, Sequence, Union
+from typing import Any, Optional, Sequence
 
-
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """function that returns the first element of a Sequence"""
     if lst:
         return lst[0]
     else:
         return None
+
